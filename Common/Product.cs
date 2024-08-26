96acdd798056b2fccd92644a9c2111c241cb65b6
Bogdan Cimpoieru <bocimpoieru@microsoft.com>
2024-08-26 11:01:31 +0300

    Add product entity

---
+++ Common/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id {  get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Stock { get; set; }
+        [DataMember]
+        public float Price { get; set; }
+    }
+}
